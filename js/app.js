623a041379df7145e488ec5c4c5fc00b26ee10ac
Jorie Fernandez <joriefefernandez@yahoo.com>
2019-04-01 17:26:00 -0700

    Created display sales in 1st and pike object

--- js/app.js
+++ js/app.js
@@ -33,63 +33,38 @@ var firstAndPike = {
 
     for (var index = 0; index < this.storeHours; index++){
       this.totalSale += Math.ceil(this.cookies[index]);
+    }   
+  },
+  
+  displaySales: function(){
+    this.initializeCustomers();
+    this.calculateCookies();
+    this.calculateTotalSale();
+
+    var elem = document.createElement('h2').textContent = this.storeName;
+    document.write(elem);  
+    //List sales
+    var ulElem = document.createElement('ul');
+
+    for(var index = 0; index < hours.length; index++){
+      var liElem = document.createElement('li');
+      liElem.textContent = hours[index] + this.cookies[index] + ' cookies';   
+      ulElem.appendChild(liElem);
     }
-    
-  }    
+
+    var totalElem = document.createElement('li');
+    totalElem.textContent = 'Total:' + this.totalSale + ' cookies';
+    ulElem.appendChild(totalElem);
+    document.body.appendChild(ulElem);
+  }
+  
 };
 
-//fill out hours 
-
-function initializeHours(){
-  console.log('initializing hours..')
-  var openTime = 6; //6 am
-  var closeTime = 20; //8 pm; 8 + 12
-  var timeLength = (closeTime - openTime)+ 1;
-
-  for (var index = 0; index < timeLength; index++){
-    if(openTime <= 12){
-      hours[index] = openTime + 'am:';
-    } else {
-      
-      hours[index] = (openTime - 12) + 'pm:';
-    }
-    openTime += 1;
-    console.log(hours[index]);
-  }
-}
 
 
-//Function to display hourly sales per location
-function displaySalesPerLocation(curStore){
 
-  curStore.initializeCustomers();
-  curStore.calculateCookies();
-  curStore.calculateTotalSale();
-
-  displayStoreName(curStore.storeName);
-  //List sales
-  var ulElem = document.createElement('ul');
-  
-
-  for(var index = 0; index < hours.length; index++){
-    var liElem = document.createElement('li');
-    liElem.textContent = hours[index] + curStore.cookies[index] + ' cookies';   
-    ulElem.appendChild(liElem);
-  }
-
-  var totalElem = document.createElement('li');
-  totalElem.textContent = 'Total:' + curStore.totalSale + ' cookies';
-  ulElem.appendChild(totalElem);
-  document.body.appendChild(ulElem);
-}
-
-//Function to display store name
-function displayStoreName(storeName){
-  var elem = document.createElement('h2').textContent = storeName;
-  document.write(elem);  
-}
+// Function calls
 
 initializeHours();
+firstAndPike.displaySales();
 
-//Display 1st and Pike info
-displaySalesPerLocation(firstAndPike);
